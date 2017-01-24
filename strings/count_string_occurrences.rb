7c956af79edeeb2fcec0a83be2068ee32014c8bc
Puiyee Tong <pytong07@gmail.com>
2017-01-24 16:36:03 -0500

    Add KMP algorithm

--- strings/count_string_occurrences.rb
+++ strings/count_string_occurrences.rb
@@ -1,30 +1,61 @@
-def count_string_occurrences
-
+STDIN.read.split("\n").split("")
+   puts a
 end
 
-def prefix_table(pattern)
-  result = [-1, 0]
-  pattern_length = pattern.length
-  i = 2
-  s = 0
 
-  while i <= pattern_length
-    # we have a continued substring in pattern from the start of pattern
-    if pattern[i - 1] == pattern[s]
-      result[i] = s + 1
-      i += 1
-      s += 1
-    # if the substring stops, fall back to the start
-    elsif s > 0
-      s = result[s]
-    # we aren't in a substring, so use the default value
-    else
-      result[i] = 0
-      i += 1
+
+def KMP(text, pattern)
+  i = 0
+  j = 0
+  count = 0
+
+  pattern_length = pattern.length
+  text_length = text.length
+
+  prefix = prefix_table(pattern)
+
+  while i < text_length
+    while j >= 0 && text[i] != pattern[j]
+      j = prefix[j]
+    end
+
+    i += 1
+    j += 1
+
+    # a match is found
+    if j == pattern_length
+      puts "found substring at index: #{i - pattern_length}"
+      count += 1
+      j = prefix[j]
     end
   end
 
-  result
+  count
 end
 
-prefix_table("ATAG")
+def prefix_table(pattern)
+  i = 0
+  j = -1
+  m = pattern.length
+
+  prefix = Array.new(m + 1)
+
+    prefix[i] = j;
+    while i < m
+      while j >= 0 && pattern[i] != pattern[j]
+        j = prefix[j]
+      end
+
+      i += 1
+      j += 1
+      prefix[i] = j
+    end
+
+  prefix
+end
+
+
+
+prefix_table("alabala")
+
+KMP("babalabalabalatheend", "alabala")
